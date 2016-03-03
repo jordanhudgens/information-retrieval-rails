a09f47661950c07bd7a59d87851f6e08dea01667
Jordan Hudgens <jordan@wow.com>
2016-03-03 10:28:47 -0600

    Added styling to tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	def convert_tags_to_links(tags: tags)
 		arr = convert_tags_to_array(tags)
 		link_string = ""
-		arr.map { |e| link_string << "<a href='questions?utf8=✓&q=#{e}'>#{e}</a>&nbsp;/&nbsp;" }
+		arr.map { |e| link_string << "<a style='font-size:1.15em;' class='label label-warning' href='questions?utf8=✓&q=#{e}'>#{e}</a>&nbsp;" }
 		link_string.html_safe
 	end
 
