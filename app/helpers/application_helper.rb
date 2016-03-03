714c1e590b080415eae66c0bb43e965c3101caf4
Jordan Hudgens <jordan@wow.com>
2016-03-03 10:25:14 -0600

    built script to convert tags to clickable links that can be converted to queries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	def convert_tags_to_links(tags: tags)
+		arr = convert_tags_to_array(tags)
+		link_string = ""
+		arr.map { |e| link_string << "<a href='questions?utf8=✓&q=#{e}'>#{e}</a>&nbsp;/&nbsp;" }
+		link_string.html_safe
+	end
+
+	def convert_tags_to_array(str)
+		str.gsub!(/(\,)(\S)/, "\\1 \\2")
+		YAML::load(str)
+	end
 end
