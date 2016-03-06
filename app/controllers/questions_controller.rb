a2dfe04f82accd3c3a1242e6a84fa08bd743ff4f
mahdinaser <mahdinaser@yahoo.com>
2016-03-06 21:00:25 +0000

    searchkick changes for Adding synonum, order , wordnet and highlights

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,10 @@
 class QuestionsController < ApplicationController
 	def index
 		query = params[:q].presence || "*" 
-		@questions = Question.search(query, suggest: true,order: {_score: :desc},fields: [{title: :text_start}], highlight: {tag: "<strong>", fields: {description: {fragment_size: 100}}},page: params[:page], per_page: 20)
-		@questions = 	@questions.first
-        @questions.similar(fields: ["title"])
+		@questions = Question.search(query, suggest: true,order: {_score: :desc},page: params[:page], per_page: 20)
+		# ,fields: [{title: :text_start}], highlight: {tag: "<strong>", fields: {description: {fragment_size: 100}}}
+# 		@questions = 	@questions.first
+#         @questions.similar(fields: ["title"])
 	end
 
   def autocomplete
