d597c4c53d1ac19478b1207e3ddc7baa7dc29bc4
Jordan Hudgens <jordan@wow.com>
2016-04-28 14:31:15 -0500

    Implemented the first portion of the history feature where previous historical searches can be added to the query

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,9 +2,20 @@ class QuestionsController < ApplicationController
 	def index
 		query = params[:q].presence || "*" 
 		@questions = Question.search(query, suggest: true,order: {_score: :desc},page: params[:page], per_page: 20)
-		
-		if current_user && query
+
+		if current_user && query && current_user.histories
 			return if query == '*'
+
+			p "*" * 500
+			p current_user.histories.order(created_at: :desc).pluck(:query_title).last(5)
+
+			@questions = Question.search(query, suggest: true,order: {_score: :desc},page: params[:page], per_page: 20)
+
+			# Add links where previous searches can be added to query
+			# questions = Question.search(query, suggest: true,order: {_score: :desc},page: params[:page], per_page: 20)
+
+			# @questions = Searchkick.multi_search([questions, current_user.histories])
+
 			History.create!(user_id: current_user.id, query_title: query)
 		end
 	end
